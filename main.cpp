15a4440f6fcf28e65fe424649dec72ec5b8ef1d2
Tuấn Vũ <97029384+tuanvu1202@users.noreply.github.com>
2024-09-09 23:44:16 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,28 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<long long, long long>;
+
+#define pb push_back
+#define ins insert
+#define fi first
+#define se second
+#define sz(x) (int)(x.size());
+#define all(x) x.begin(), x.end()
 #define FOR(i, a, b) for (int i = (a); i <= (b); i++)
 #define FOD(i, a, b) for (int i = (a); i >= (b); i--)
 #define REP(i, n) for (int i = 0; i < (n); i++)
 
-using namespace std;
+const int MOD = 1e9 + 7; //998244353
+const long long INF = 1e9;
+const int N = 1e5 + 10;
 
-const int maxN = 2e5 + 5;
-
-int main(){
-	ios_base::sync_with_stdio(false);cin.tie(nullptr);
+int main()
+{
+    ios_base::sync_with_stdio(false);cin.tie(nullptr);
 	
-	return 0;
+    return 0;
 }
