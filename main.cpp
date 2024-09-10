4be84029504fbc7d11ff38f20315e47d0e50237c
Tuấn Vũ <97029384+tuanvu1202@users.noreply.github.com>
2024-09-10 11:47:41 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,9 +12,9 @@ using pll = pair<long long, long long>;
 #define se second
 #define sz(x) (int)(x.size());
 #define all(x) x.begin(), x.end()
-#define FOR(i, a, b) for (int i = (a); i <= (b); i++)
-#define FOD(i, a, b) for (int i = (a); i >= (b); i--)
-#define REP(i, n) for (int i = 0; i < (n); i++)
+#define f0(i, a, b) for (int i = (a); i <= (b); i++)
+#define f1(i, a, b) for (int i = (a); i >= (b); i--)
+#define f2(i, n) for (int i = 0; i < (n); i++)
 
 const int MOD = 1e9 + 7; //998244353
 const long long INF = 1e9;
@@ -23,6 +23,7 @@ const int N = 1e5 + 10;
 int main()
 {
     ios_base::sync_with_stdio(false);cin.tie(nullptr);
-	
-    return 0;
+    
+    
+	return 0;
 }
